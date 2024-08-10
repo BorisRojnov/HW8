9660b8a21a2c2b0d36acc14946de99011bf0c339
BorisRojnov <BorisRojnov@yandex.by>
2024-08-10 13:42:35 +0300

    homework complete

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+        System.out.println("Развернуть массив\n");
+        for(int i = 0; i < array.length/2; i++) {
+             int helpSpace = array[i];
+             array[i] = array[array.length - (1 + i)];
+             array[array.length - (1 + i)] = helpSpace;
+        }
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
 
-        //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
 
-        //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
     }
 }
